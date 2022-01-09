4d043d9eea87f9267d441d82acf905616ae2ff14
vuong <nguyenvuong1122000@gmail.com>
2022-01-09 20:22:36 +0700

    add yolov5

---
+++ DL-Yolov5/yolo.py
@@ -0,0 +1,35 @@
+import yolov5
+import torch
+from PIL import Image
+
+def yolo_detect(image_path):
+    # pip install yolov5
+    # load model
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model = yolov5.load('last.pt').to(device)
+
+    # set image, you can use api call in that
+    # img = 'DL-Yolov5/results/zidane.jpg'
+    img = image_path
+
+    # perform inference
+    results = model(img)
+
+    # inference with larger input size
+    results = model(img, size=1280)
+
+    # inference with test time augmentation
+    results = model(img, augment=True)
+
+    # parse results
+    predictions = results.pred[0]
+    boxes = predictions[:, :4]  # x1, x2, y1, y2
+    scores = predictions[:, 4]
+    categories = predictions[:, 5]
+
+    # show detection bounding boxes on image
+    results.show()
+
+    # save results into "results/" folder, use result
+    results.save(save_dir='results/')
